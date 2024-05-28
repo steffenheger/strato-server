828f29b8206221790057057b485c515552884593
Steffen Heger <steffen.heger@gmail.com>
2024-05-28 11:44:50 +0200

    wip

--- docker/up.sh
+++
@@ -1,9 +0,0 @@
-# obsolete, replaced by docker-compose
-
-docker stop $(docker ps -a -q)
-
-docker build -t nginx-reverse-proxy /srv/nginx/
-docker build -t vinoso /srv/vinoso/
-
-docker run -d -p 80:80 nginx-reverse-proxy
-docker run -d -p 8000:8000 vinoso
